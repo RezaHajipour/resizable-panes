92bf2b78415a3a0f64253dcc78a438175deee685
reza <rezahajipour@gmail.com>
2022-07-01 11:56:52 +0200

    touchscreen removed

--- index.js
+++ index.js
@@ -4,35 +4,24 @@
     var $frontImage = $panes.eq(1);
     var $slider = $(".slider");
 
-    //---------for touchscreen devices--------
+    let isSliding = false;
+    $wrapper.on("mousemove", (e) => {
+        if (e.target.className === "slider") {
+            return;
+        }
+        if (isSliding) {
+            $wrapper.css("cursor", "ew-resize");
+            $frontImage.css("width", e.offsetX);
+            $slider.css("left", e.offsetX - 5);
+        }
+    });
 
-    //--------------
+    $wrapper.on("mouseup mouseleave", (e) => {
+        isSliding = false;
+        $wrapper.css("cursor", "default");
+    });
 
-    //     let isSliding = false;
-    //     $wrapper.on("mousemove", (e) => {
-    //         if (e.target.className === "slider") {
-    //             return;
-    //         }
-    //         if (isSliding) {
-    //             $wrapper.css("cursor", "ew-resize");
-    //             $frontImage.css("width", e.offsetX);
-    //             $slider.css("left", e.offsetX - 5);
-    //         }
-    //     });
-
-    //     $wrapper.on("mouseup mouseleave ", (e) => {
-    //         e.preventDefault();
-    //         isSliding = false;
-    //         $wrapper.css("cursor", "default");
-    //     });
-
-    //     $slider.on("mousedown ", (e) => {
-    //         e.preventDefault();
-    //         isSliding = true;
-    //     });
-    // })();
-    document.querySelector(".wrapper").addEventListener("touchstart", func);
-    function func(e) {
-        console.log(e.touches, e.type);
-    }
+    $slider.on("mousedown", (e) => {
+        isSliding = true;
+    });
 })();
